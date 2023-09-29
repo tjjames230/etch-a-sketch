f3a1edb46b7818f74bed8d3227d06467be187b1f
tjjames230 <108950500+tjjames230@users.noreply.github.com>
2023-09-29 14:59:41 -0500

    beginning to create logic for updating canvas size based on input value

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 console.log("hi");
+
+const input = document.querySelector("input");
+const inputTxt = document.querySelector("#input-txt");
+
+input.addEventListener("click", (e) => {
+  // Update input text
+  inputTxt.innerText = `${e.target.value} x ${e.target.value}`;
+
+  // Update canvas to allow target value of rows
+
+  // Create row of divs in canvas equal to target value
+  for (let x = 0; x <= e.target.value; x++) {
+    // Create column of divs in canvas equal to target value
+    for (let y = 0; y <= e.target.value; y++) {}
+  }
+});
