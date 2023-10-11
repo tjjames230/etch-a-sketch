33ab4238dda46a7f2a9f9e0b3156d966202eff9a
tjjames230 <108950500+tjjames230@users.noreply.github.com>
2023-10-11 13:29:51 -0500

    added functionality to update canvas grid based on input value

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ console.log("hi");
 
 const input = document.querySelector("input");
 const inputTxt = document.querySelector("#input-txt");
+const canvas = document.querySelector("#canvas");
 
 input.addEventListener("click", (e) => {
   // Update input text
   inputTxt.innerText = `${e.target.value} x ${e.target.value}`;
 
   // Update canvas to allow target value of rows
+  canvas.style.gridTemplateColumns = `repeat(${e.target.value}, 1fr)`;
 
   // Create row of divs in canvas equal to target value
   for (let x = 0; x <= e.target.value; x++) {
